215c8fd0455e43d25bfd8652cebdfb71991df3c3
Leonardo Toccaceli <leotoccaceli@gmail.com>
2020-08-25 19:48:57 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    "src/**/*.js",
+    "src/**/*.jsx",
+    "src/**/*.ts",
+    "src/**/*.tsx",
+    "public/**/*.html",
+  ],
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
