98a1f532980504f3ece382f96ecf8029e7fe0a61
Leonardo Toccaceli <leotoccaceli@gmail.com>
2021-01-20 16:55:54 -0300

    Minor changes

--- src/utilities/useKeyPress.js
+++ src/utilities/useKeyPress.js
@@ -27,6 +27,7 @@ export const useKeyPress = (targetKey) => {
       window.removeEventListener("keydown", downHandler);
       window.removeEventListener("keyup", upHandler);
     };
+  // eslint-disable-next-line
   }, []); // Empty array ensures that effect is only run on mount and unmount
 
   return keyPressed;
